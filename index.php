cc2b1c5ee0666ff82be9e70fbc08893d7800f927
iwanami-intwk <iwanami@intwk.co.jp>
2017-04-05 11:09:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
 				<p class='description'>Thanks for creating a <span class="blue">PHP Starter Application</span>.</p> 
 				<?php
-				$hello="Hello World";
+				$hello="Hello World?!";
 				print ($hello);
 				?>
 			</td>
